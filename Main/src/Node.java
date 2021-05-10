df48dc3d9c218369706715be7904285d763beacb
alexander <alexander_5560@hotmail.com>
2021-05-10 10:27:55 +0200

    Added main node class added default makeConnection function.

--- Main/src/Node.java
+++ Main/src/Node.java
@@ -1,2 +1,8 @@
-public class Node {
+public class Node implements NodeInterface {
+    private Node node1;
+    private Node node2;
+
+    public String makeConnection(char Letter) {
+        return "Dit zou niet moeten gebeuren.";
+    }
 }
