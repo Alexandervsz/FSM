4af8af5350dabe5a1f6dc38144c2a593f5a011b8
alexander <alexander_5560@hotmail.com>
2021-05-10 11:31:14 +0200

    Added node logic, changed currentnode to nodename, removed output as parameter, added node A and B as parameters.

--- Main/src/Node.java
+++ Main/src/Node.java
@@ -1,8 +1,27 @@
 public class Node implements NodeInterface {
-    private Node node1;
-    private Node node2;
+    private String nodeName;
+    private Node nodeA;
+    private Node nodeB;
 
-    public String makeConnection(char Letter) {
-        return "Dit zou niet moeten gebeuren.";
+    public String makeConnection(String input, StringBuilder output) {
+        if (input.charAt(0) == 'A') {
+            input = input.substring(1);
+            output.append(nodeName);
+            nodeA.makeConnection(input, output);
+
+        }
+        else if (input.charAt(0) == 'B'){
+            input = input.substring(1);
+            output.append(nodeName);
+            if (nodeB != null){
+            nodeB.makeConnection(input, output);}
+            else{
+                return "Kwam bij s2 met een A, dus gestopt.";
+            }
+        }
+        else {
+            return "Kwam bij ongeldige letter dus gestopt\n afgelopen pad:"+ output.toString();
+        }
+        return "Mysterious error";
     }
 }
