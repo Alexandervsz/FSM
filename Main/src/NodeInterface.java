86875a19ce08b00f185adcbb02d98b25b6aa92cd
alexander <alexander_5560@hotmail.com>
2021-05-12 16:30:27 +0200

    Renamed makeConnection to connect. Replaced Node A/B with map. Added update function for map. Removed interface.

--- Main/src/NodeInterface.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public interface NodeInterface {
-    void setNodeA(Node nodeA);
-
-    void setNodeB(Node nodeB);
-
-    void makeConnection(String input, ArrayList<String> output);
-}
