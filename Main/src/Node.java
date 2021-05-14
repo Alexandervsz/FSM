bf8306968eb1c73e886ffa84a5ee31fa9e8e62d4
alexander <alexander_5560@hotmail.com>
2021-05-14 13:32:11 +0200

    Removed the need to pass new arraylist to makeConnection, instead made it parameter of Node.

--- Main/src/Node.java
+++ Main/src/Node.java
@@ -5,6 +5,11 @@ import java.util.Map;
 public class Node {
     private final String nodeName;
     private final Map<String, Node> nodeConnections = new HashMap<>();
+    ArrayList<String> output = new ArrayList<>();
+
+    public void setOutput(ArrayList<String> output) {
+        this.output = output;
+    }
 
     public Node(String nodeName) {
         this.nodeName = nodeName;
@@ -16,14 +21,15 @@ public class Node {
     }
 
 
-    public Map<String, ArrayList<String>> connect(String input, ArrayList<String> output) {
+    public Map<String, ArrayList<String>> connect(String input) {
         /* This function tells the node whether to go to a next node or not. */
         output.add(nodeName);
         if (input.length() > 0) {
             if (nodeConnections.containsKey(input.substring(0, 1))) {
                 Node new_node = nodeConnections.get(input.substring(0, 1));
                 input = input.substring(1);
-                new_node.connect(input, output);
+                new_node.setOutput(output);
+                new_node.connect(input);
             } else {
                 Map<String, ArrayList<String>> outputMap = new HashMap<>();
                 outputMap.put("Kwam bij ongeldige letter, dus gestopt.\nAfgelopen route: ", output);
