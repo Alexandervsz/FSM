fe17eb8eeb9b52a8182c575f5a2f55c51bd982d4
Alexandervsz <alexander_5560@hotmail.com>
2021-05-15 19:34:51 +0200

    Improved variable names, added more test cases.

--- Test/src/Tests.java
+++ Test/src/Tests.java
@@ -1,4 +1,6 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,16 +10,59 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Tests {
 
     @Test
-    public void testNode(){
-        Node s0 = Main.initialiseNodes();
-        Map<String, ArrayList<String>> nodeMap = s0.connect("A");
+    public void testNode() {
+        Map<String, ArrayList<String>> testOne = new HashMap<>();
+        ArrayList<String> testListOne = new ArrayList<>();
+        testListOne.add("s0");
+        testListOne.add("s2");
+        testOne.put("1", testListOne);
 
-        ArrayList<String> output = new ArrayList<>();
-        output.add("s0");
-        output.add("s2");
-        Map<String, ArrayList<String>> testMap = new HashMap<>();
-        testMap.put("Letters op.\nAfgelopen route: ", output);
-        assertEquals(nodeMap, testMap);
+
+        Map<String, ArrayList<String>> testTwo = new HashMap<>();
+        ArrayList<String> testListTwo = new ArrayList<>();
+        testListTwo.add("s0");
+        testListTwo.add("s1");
+        testListTwo.add("s1");
+        testListTwo.add("s1");
+        testListTwo.add("s2");
+        testTwo.put("1", testListTwo);
+
+
+        Map<String, ArrayList<String>> testThree = new HashMap<>();
+        ArrayList<String> testListThree = new ArrayList<>();
+        testListThree.add("s0");
+        testListThree.add("s2");
+        testThree.put("0", testListThree);
+
+
+        Map<String, ArrayList<String>> testFour = new HashMap<>();
+        ArrayList<String> testListFour = new ArrayList<>();
+        testListFour.add("s0");
+        testListFour.add("s2");
+        testFour.put("0", testListFour);
+
+
+        Map<String, ArrayList<String>> testFive = new HashMap<>();
+        ArrayList<String> testListFive = new ArrayList<>();
+        testListFive.add("s0");
+        testFive.put("0", testListFive);
+
+
+        Map<String, ArrayList<String>> testSix = new HashMap<>();
+        ArrayList<String> testListSix = new ArrayList<>();
+        testListSix.add("s0");
+        testSix.put("1", testListSix);
+
+
+        Assertions.assertAll(
+                () -> assertEquals(FiniteStateMachine.runNodes("A"), testOne),
+                () -> assertEquals(FiniteStateMachine.runNodes("BAAB"), testTwo),
+                () -> assertEquals(FiniteStateMachine.runNodes("AA"), testThree),
+                () -> assertEquals(FiniteStateMachine.runNodes("AX"), testFour),
+                () -> assertEquals(FiniteStateMachine.runNodes("1453"), testFive),
+                () -> assertEquals(FiniteStateMachine.runNodes(""), testSix)
+
+        );
     }
 
 }
