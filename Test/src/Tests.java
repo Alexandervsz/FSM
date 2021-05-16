b01837cf7d0715e16e7f57da1f8a31847ac80b41
Alexandervsz <alexander_5560@hotmail.com>
2021-05-16 21:52:00 +0200

    Changed strings to booleans.

--- Test/src/Tests.java
+++ Test/src/Tests.java
@@ -11,47 +11,47 @@ public class Tests {
 
     @Test
     public void testNode() {
-        Map<String, ArrayList<String>> testOne = new HashMap<>();
+        Map<Boolean, ArrayList<String>> testOne = new HashMap<>();
         ArrayList<String> testListOne = new ArrayList<>();
         testListOne.add("s0");
         testListOne.add("s2");
-        testOne.put("1", testListOne);
+        testOne.put(true, testListOne);
 
 
-        Map<String, ArrayList<String>> testTwo = new HashMap<>();
+        Map<Boolean, ArrayList<String>> testTwo = new HashMap<>();
         ArrayList<String> testListTwo = new ArrayList<>();
         testListTwo.add("s0");
         testListTwo.add("s1");
         testListTwo.add("s1");
         testListTwo.add("s1");
         testListTwo.add("s2");
-        testTwo.put("1", testListTwo);
+        testTwo.put(true, testListTwo);
 
 
-        Map<String, ArrayList<String>> testThree = new HashMap<>();
+        Map<Boolean, ArrayList<String>> testThree = new HashMap<>();
         ArrayList<String> testListThree = new ArrayList<>();
         testListThree.add("s0");
         testListThree.add("s2");
-        testThree.put("0", testListThree);
+        testThree.put(false, testListThree);
 
 
-        Map<String, ArrayList<String>> testFour = new HashMap<>();
+        Map<Boolean, ArrayList<String>> testFour = new HashMap<>();
         ArrayList<String> testListFour = new ArrayList<>();
         testListFour.add("s0");
         testListFour.add("s2");
-        testFour.put("0", testListFour);
+        testFour.put(false, testListFour);
 
 
-        Map<String, ArrayList<String>> testFive = new HashMap<>();
+        Map<Boolean, ArrayList<String>> testFive = new HashMap<>();
         ArrayList<String> testListFive = new ArrayList<>();
         testListFive.add("s0");
-        testFive.put("0", testListFive);
+        testFive.put(false, testListFive);
 
 
-        Map<String, ArrayList<String>> testSix = new HashMap<>();
+        Map<Boolean, ArrayList<String>> testSix = new HashMap<>();
         ArrayList<String> testListSix = new ArrayList<>();
         testListSix.add("s0");
-        testSix.put("1", testListSix);
+        testSix.put(true, testListSix);
 
 
         Assertions.assertAll(
