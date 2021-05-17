9b661f3435b2ce92ba68efee0096d6771f8203fe
alexander <alexander_5560@hotmail.com>
2021-05-17 10:59:50 +0200

    - Added waitForUser methode which waits for the user to press enter. - Added runNodesGame which starts the nodes in the "game" mode, and handles them correctly. - Added call to runNodesGame in main.

--- Main/src/FiniteStateMachine.java
+++ Main/src/FiniteStateMachine.java
@@ -1,14 +1,12 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class FiniteStateMachine {
     public static void main(String[] args) {
-        runFsmString();
+        runNodesGame();
+        //runFsmString();
     }
 
-    public static void runFsmString(){
+    public static void runFsmString() {
         System.out.println("Voer hier je input in: ");
         Scanner scanner2 = new Scanner(System.in);
         String input = scanner2.nextLine().toUpperCase();
@@ -37,6 +35,50 @@ public class FiniteStateMachine {
         return outputMap;
     }
 
+    public static void runNodesGame() {
+        Node currentNode = initialiseNodesGame();
+        Random rand = new Random();
+        int hurtCounter = 0;
+        int hitCounter = 0;
+        while (!currentNode.getNodeName().equals("Dead") || !currentNode.getNodeName().equals("Won")) {
+            switch (currentNode.getNodeName()) {
+                case "Initial" -> {
+                    waitForUser("Welkom, druk op enter om verder te gaan");
+                    currentNode.connect(1);
+                    currentNode = currentNode.getCurrentNode();
+                }
+                case "Fight" -> {
+                    waitForUser("Druk op enter om aan te vallen.");
+                    currentNode.connect(rand.nextInt(2));
+                    currentNode = currentNode.getCurrentNode();
+                }
+                case "Hit" -> {
+                    waitForUser("Je hebt de vijand geraakt! Druk op enter om verder te gaan.");
+                    hitCounter++;
+                    currentNode.connect(hitCounter);
+                    currentNode = currentNode.getCurrentNode();
+                }
+                case "Hurt" -> {
+                    waitForUser("Je bent geraakt! Druk op enter om verder te gaan.");
+                    hurtCounter++;
+                    currentNode.connect(hurtCounter);
+                    currentNode = currentNode.getCurrentNode();
+                }
+            }
+        }
+        switch (currentNode.getNodeName()) {
+            case "Won" -> waitForUser("Je hebt gewonnen, gefeliciteerd! Druk op enter om af te sluiten.");
+            case "Dead" -> waitForUser("Je bent dood, rust in vrede. Druk op enter om af te sluiten.");
+        }
+
+    }
+
+    public static void waitForUser(String message){
+        System.out.println(message);
+        Scanner scanner2 = new Scanner(System.in);
+        scanner2.nextLine();
+    }
+
     public static Node initialiseNodesString() {
         Node s0 = new Node("s0");
         Node s1 = new Node("s1");
@@ -55,7 +97,7 @@ public class FiniteStateMachine {
     public static Node initialiseNodesGame() {
         Node initial = new Node("Initial");
         Node fight = new Node("Fight");
-        Node hurt = new Node("Fight");
+        Node hurt = new Node("Hurt");
         Node dead = new Node("Dead");
         Node hit = new Node("Hit");
         Node won = new Node("Won");
@@ -71,4 +113,5 @@ public class FiniteStateMachine {
 
         return initial;
     }
+
 }
